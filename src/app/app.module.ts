cabb38fcb13b3ff649341d77047f26c90a91de63
nicoacos7a <nicoacos7a@gmail.com>
2020-05-23 23:01:07 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
+import { MockArticulosFamiliasService } from './services/mock-articulos-familias.service';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ArticulosFamiliasService } from './services/articulos-familias.service';
+
+import { RouterModule } from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
+import { MenuComponent } from './components/menu/menu.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
+import { MockArticulosService } from './services/mock-articulos.service';
+
+import { ReactiveFormsModule } from "@angular/forms";
+
+
+@NgModule({
+  imports:
+  [ 
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'inicio', component: InicioComponent },
+      { path: 'articulosfamilias', component: ArticulosFamiliasComponent },
+      { path: 'articulos', component: ArticulosComponent },
+      { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+      ]),
+      ReactiveFormsModule,
+    ],
+  declarations: 
+  [ 
+    AppComponent, 
+    InicioComponent, 
+    ArticulosFamiliasComponent, 
+    MenuComponent, 
+    ArticulosComponent 
+  ],
+  bootstrap:
+  [
+    AppComponent
+  ],
+  providers:
+  [
+    MockArticulosFamiliasService, 
+    ArticulosFamiliasService,  
+    { provide: APP_BASE_HREF, useValue: "/", providers: [MockArticulosService] }
+  ]
+})
+export class AppModule { }
